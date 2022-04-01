1466bedba709344f4bad33d6083134b2f96511e5
Andrei Lavrov <lavrovvandrei@gmail.com>
2022-04-01 10:11:37 +0300

    feature/server/ add login route and authorisation token

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,7 +1,9 @@
-const Router = require("express")
-const User = require("../models/User")
-const bcryptjs = require("bcryptjs")
-const { check, validationResult } = require("express-validator")
+const Router = require('express')
+const User = require('../models/User')
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require("config")
+const { check, validationResult } = require('express-validator')
 const router = new Router()
 
 
@@ -24,7 +26,7 @@ router.post('/registration',
                 return res.status(400).json({ message: `User with email ${email} already exists` })
             }
 
-            const hashPassword = await bcryptjs.has(password, 15)
+            const hashPassword = await bcryptjs.hash(password, 7)
             const user = new User({ email, password: hashPassword })
 
             await user.save()
@@ -32,7 +34,39 @@ router.post('/registration',
             return res.json({ message: 'User was created' })
         } catch (e) {
             console.log(e)
-            res.send({message: "Server error"})
+            res.send({message: 'Server error'})
+        }
+    })
+
+router.post('/login',
+    async (req, res) => {
+        try {
+            const { email, password } = req.body
+            const user = await User.findOne({ email })
+
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' })
+            }
+
+            const isPasswordValid = bcryptjs.compareSync(password, user.password)
+            if (!isPasswordValid) {
+                return res.status(400).json({ message: 'Wrong password' })
+            }
+
+            const token = jwt.sign({ id: user.id }, config.get('secretKey'), { expiresIn: '1h' })
+            return res.json({
+                token,
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    diskSpace: user.diskSpace,
+                    usedSpace: user.usedSpace,
+                    avatar: user.avatar
+                }
+            })
+        } catch (e) {
+            console.log(e)
+            res.send({message: 'Server error'})
         }
     })
 
