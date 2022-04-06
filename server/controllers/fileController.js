6b938a55407d483c0934d62affac6860696ca004
Andrei Lavrov <lavrovvandrei@gmail.com>
2022-04-06 22:52:47 +0300

    feature/server/ add download file logic

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -83,15 +83,16 @@ class FileController {
 
   async downloadFile(req, res) {
     try {
-      const file = await File.findOne({_id: req.query.id, user: req.user.id})
-      const path = config.get('filePath') + '\\' + req.user.id + '\\' + file.path + '\\' + file.name;
+      const file = await File.findOne({ _id: req.query.id, user: req.user.id });
+      const path =
+        config.get('filePath') + '\\' + req.user.id + '\\' + file.path + '\\' + file.name;
       if (fs.existsSync(path)) {
-        return res.download(path, file.name)
+        return res.download(path, file.name);
       }
-      return res.status(400).json({message: "Download error"})
+      return res.status(400).json({ message: 'Download error' });
     } catch (e) {
-      console.log(e)
-      res.status(500).json({message: "Download error"})
+      console.log(e);
+      res.status(500).json({ message: 'Download error' });
     }
   }
 }
