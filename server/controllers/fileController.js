04b0f25d22378eeb78e77eb069a1fd1671612a3e
Andrei Lavrov <lavrovvandrei@gmail.com>
2022-04-07 14:27:05 +0300

    feature/server/ add deleting file(and folder) logic

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -61,12 +61,16 @@ class FileController {
 
       file.mv(path);
 
+      let filePath = file.name;
+      if (parent) {
+        filePath = parent.path + '\\' + file.name;
+      }
       const type = file.name.split('.').pop();
       const dbFile = new File({
         name: file.name,
         type,
         size: file.size,
-        path: parent?.path,
+        path: filePath,
         parent: parent?._id,
         user: user._id,
       });
@@ -93,6 +97,22 @@ class FileController {
     } catch (e) {
       console.log(e);
       res.status(500).json({ message: 'Download error' });
+    }
+  }
+
+  async deleteFile(req, res) {
+    try {
+      const file = await File.findOne({ _id: req.query.id, user: req.user.id });
+      if (!file) {
+        return res.status(400).json({ message: 'File not found' });
+      }
+      fileService.deleteFile(file);
+      await file.remove();
+
+      return res.json({ message: 'File was deleted' });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({ message: 'Deleting error' });
     }
   }
 }
