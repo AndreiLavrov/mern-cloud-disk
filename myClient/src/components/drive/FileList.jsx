499359decec3feead73b3e2ad504565163511337
Andrei Lavrov <lavrovvandrei@gmail.com>
2022-04-08 00:11:30 +0300

    feature/client/ add animation

--- myClient/src/components/drive/FileList.jsx
+++ myClient/src/components/drive/FileList.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-
 import './fileList.css';
+import { useSelector } from 'react-redux';
 import File from './File';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const FileList = () => {
-  const files = useSelector((state) => state.files.files).map((file) => (
-    <File key={file._id} file={file} />
-  ));
+  const files = useSelector((state) => state.files.files);
 
   return (
     <div className="filelist">
@@ -16,7 +14,13 @@ const FileList = () => {
         <div className="filelist__date">Дата</div>
         <div className="filelist__size">Размер</div>
       </div>
-      {files}
+      <TransitionGroup>
+        {files.map((file) => (
+          <CSSTransition key={file._id} timeout={500} classNames={'file'} exit={false}>
+            <File file={file} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 };
