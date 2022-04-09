4843c2830eac57a8e924f244a482ce717d912df0
Andrei Lavrov <lavrovvandrei@gmail.com>
2022-04-09 10:59:55 +0300

    bugfix/server/ add "profile" router

--- server/routes/profile.routes.js
+++ server/routes/profile.routes.js
@@ -4,7 +4,7 @@ const router = new Router();
 const authMiddleware = require('../middleware/auth.middleware.js');
 const fileController = require('../controllers/fileController.js');
 
-router.post('', authMiddleware, fileController.uploadAvatar);
-router.delete('', authMiddleware, fileController.deleteAvatar);
+router.post('/avatar', authMiddleware, fileController.uploadAvatar);
+router.delete('/avatar', authMiddleware, fileController.deleteAvatar);
 
 module.exports = router;
